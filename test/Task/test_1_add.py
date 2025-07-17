04bd33ae099fdf61b4ce552155ee70c8a69a07d6
RuslanErg <ruslan.bel.2013@gmail.com>
2025-07-17 20:54:44 +0300

    111

---
+++ test/Task/test_1_add.py
@@ -0,0 +1,15 @@
+import requests
+
+def test_add():
+    body = {"title":"тестовая задача","completed":False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+
+    assert response.status_code == 200
+
+    id = response.json()['id']
+
+    body = {"completed": True}
+    response = requests.patch(f"https://todo-app-sky.herokuapp.com/{id}", json=body)
+
+    assert response.status_code == 200
+    assert response.json()["completed"] == True
