20f574b157d7080f42f7b32262a2d60935ff49d2
dahenkakyd <dahenkakyd@gmail.com>
2025-07-17 20:53:03 +0300

    commit-1

---
+++ test/delate_test.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def test_delete():
+    body = {"title": "generated", "completed": False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+
+    assert response.status_code == 200
+
+    id = response.json()["id"]
+
+    delete_response = requests.delete(f'https://todo-app-sky.herokuapp.com/{id}')
+    # assert delete_response.status_code == 404
+
+    response = requests.get(f"https://todo-app-sky.herokuapp.com/{id}")
+    assert response.status_code == 404
